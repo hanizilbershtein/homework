775da75b0764ad763f24c919c6d528f2a97e86f9
Your Name <you@example.com>
2022-02-06 22:45:26 +0200

    newnumber

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import Number from "./Components/Number";
+import {Number,zoogi} from "./Components/Number";
+
+
+
 function App() {
   return (
     <div className="App">
@@ -12,24 +15,7 @@ function App() {
     </div>
   );
 }
-const zoogi= nbr=> {
-if(nbr==2||nbr==3||nbr==5||nbr==7){
-  return "red"
-  }
-else if(nbr%2==0){
-  return "green"  
-}
 
-else if(nbr%3==0||nbr%5==0||nbr%7==0||nbr==1){
-  return "yellow"
-}
-
-else{
-  return "red"
-
-}
-
-}
 
 let arr=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 
--- src/Components/Number.js
+++ src/Components/Number.js
@@ -1,16 +1,44 @@
 import React from 'react'
+import { useState } from 'react';
 
-const Number = ({color,num}) => {
-    return (
-        <div className={color}>
-         <p>{num}</p> 
-        </div>
-    )
+
+let color=""
+function zoogi(nbr) {
+    if(nbr==2||nbr==3||nbr==5||nbr==7){
+       color="red"
+      }
+    else if(nbr%2==0){
+      color= "green"  
+    }
+    
+    else if(nbr%3==0||nbr%5==0||nbr%7==0||nbr==1){
+      color= "yellow"
+    }
+    
+    else{
+      return "red"
+    
+    }
+    return color
+    
 }
-
-
-
-
-
-
-export default Number
+    const Number = ({color,num}) => {
+      let [newNumber, setNewNumber] = useState(num)
+      const[newClass,setnewClass]=useState(color)
+  
+  
+      const change = () =>{
+          setNewNumber(newNumber+=Math.floor(Math.random()*11))
+          setnewClass(zoogi(newNumber))
+  
+      }
+      return (
+           <button className={newClass}  onClick={change}>
+               <div>{newNumber}</div>
+               </button> 
+          
+      )
+  }
+    
+    
+export {Number,zoogi}
